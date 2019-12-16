be016b1a02feaa2bf7b91afe5fb3455a5e667d9e
DJ Chemik <dj-chemik@wp.pl>
2019-12-16 23:25:15 +0100

    Add logo to start screen

--- helikopter.py
+++ helikopter.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import os
 def napisz(tekst, x, y, rozmiarCzcionki, czyWysrodkowany=True):
     cz=pygame.font.SysFont("Arial", rozmiarCzcionki,0,0)
     rend=cz.render(tekst,1,(255,100,100))
@@ -15,11 +16,15 @@ szer = 600
 wys = 600
 screen = pygame.display.set_mode((szer,wys))
 
-
+copokazuje = "menu"
 while True:
     for event in pygame.event.get(): #przejdź po wszystkich elementach listy zdarzeń
         if event.type==pygame.QUIT: #jeśli naciśniemy klawisz X w oknie
             pygame.quit() #zamyka okno gry
             #quit() #zamyka IDLE
-    napisz("Naciśnij Spacje by zacząć",80,150,20,True)
+    if copokazuje=="menu":
+        napisz("Naciśnij Spacje by zacząć",80,150,20,True)
+        grafika = pygame.image.load(os.path.join('logo.png'))
+        screen.blit(grafika, (80,30))
     pygame.display.update()
+    
